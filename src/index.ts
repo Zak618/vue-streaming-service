a5b2d79229eb161ddb1a7be5e1ed8636ed74eff1
Rolan <89405318+Zak618@users.noreply.github.com>
2023-11-01 22:58:57 +0300

    Set up the project.

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import logger from 'morgan'
+
+//routes
+import streamRouter from './modules/stream/stream.controller'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+app.use('/stream', streamRouter)
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log('Начинаем стримить');
+    console.log(`http://localhost:${PORT}`);
+})
